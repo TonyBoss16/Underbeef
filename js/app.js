7b019fa944d86ac4029645a7a0575e9c66bfce3f
Anthony Contreras <40922018+TonyBoss16@users.noreply.github.com>
2022-06-13 02:19:44 -0400

    Responsive hecho

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
+'use strict';
+var btn = document.querySelector('.header__btn');
+var nav = document.querySelector('.header__nav');
+var rrss = document.querySelector('.header__rrss');
 var claim = document.querySelector('.footer__extra');
 claim.addEventListener('click', function () {
     claim.classList.toggle('active');
+});
+/*
+    Cuando hago click en .header__btn
+        .header__nav ToggerClass 'activo'
+        .header__rrss ToggerClass 'activo'
+
+*/
+btn.addEventListener('click', function () {
+    nav.classList.toggle('activo');
+    rrss.classList.toggle('activo');
 });
--- ts/app.ts
+++ ts/app.ts
@@ -1,5 +1,21 @@
+'use strict'
+
+const btn = document.querySelector('.header__btn') as HTMLButtonElement;
+const nav = document.querySelector('.header__nav') as HTMLElement;
+const rrss = document.querySelector('.header__rrss') as HTMLUListElement;
 const claim = (document.querySelector('.footer__extra') as HTMLParagraphElement);
 
 claim.addEventListener('click', ():void => {
     claim.classList.toggle('active');
-})
+});
+
+/*
+    Cuando hago click en .header__btn 
+        .header__nav ToggerClass 'activo' 
+        .header__rrss ToggerClass 'activo'
+
+*/
+    btn.addEventListener('click', () => {
+        nav.classList.toggle('activo');
+        rrss.classList.toggle('activo');
+    })
